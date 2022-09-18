06203258d8c87fc4aa7bc30acadf5131c36718b8
burakteke <burakteke37@gmail.com>
2022-09-18 22:52:59 +0300

    Controller'ların her Action Methodunda status code'lara göre ayrı ayrı return'ler yazmak yerine CustomBaseController'da bu işi ortak yapabilecek bir method oluşturuldu. CourseController buna göre düzenlendi.

---
+++ Services/Catalog/FreeCourses.Services.Catalog/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using FreeCourses.Services.Catalog.Services;
+using FreeCourses.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourses.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CourseController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        internal CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+            return CreateActionResultInstance(response);
+        }
+    }
+}
---
+++ Shared/FreeCourses.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreeCourses.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourses.Shared.ControllerBases
+{
+    public class CustomBaseController: ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            }
+        }
+    }
+}
