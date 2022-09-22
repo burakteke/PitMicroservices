2fd58ea2366343616d14c786a12eac6412637dc3
burakteke <burakteke37@gmail.com>
2022-09-23 01:47:27 +0300

    IdentityServer UserController ve SignUp methodu eklendi. Bazı modeller eklendi

---
+++ IdentityServer/FreeCourses.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using FreeCourses.IdentityServer.Dtos;
+using FreeCourses.IdentityServer.Models;
+using FreeCourses.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourses.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+            if (result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+    }
+}
