4e05c772f2605e412cdc37fdb57e6bf00e0a049c
burakteke <burakteke37@gmail.com>
2022-09-27 22:49:59 +0300

    PhotoStockAPI

---
+++ Services/PhotoStock/FreeCourses.Services.PhotoStock/Dtos/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCourses.Services.PhotoStock.Dtos
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+    }
+}
---
+++ Services/PhotoStock/FreeCourses.Services.PhotoStock/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_photo_stock";
+    options.RequireHttpsMetadata = false; //uygulamayý https yapmadýk diye. Https uygulamalar için gerek yok.
+});
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseStaticFiles();
+app.UseAuthorization();
+app.UseAuthentication();
+app.MapControllers();
+
+app.Run();
