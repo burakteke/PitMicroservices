2c62d90bc210f1697cb5d437a91184765317a061
burakteke <burakteke37@gmail.com>
2022-10-17 19:30:45 +0300

    FrontEnd SignIn ve settings

---
+++ FrontEnds/FreeCourses.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeCourses.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FrontEnds/FreeCourses.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourses.Shared.Dtos;
+using FreeCourses.Web.Models;
+using IdentityModel.Client;
+
+namespace FreeCourses.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
